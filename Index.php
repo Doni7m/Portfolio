965314789aad454f93bd12d4b5a2c7966ca01c2c
DONIZETH MARTINS DA SILVA FILHO <109109447+Doni7m@users.noreply.github.com>
2024-03-20 20:27:05 +0000

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Doni'sPortfolio</title>
+    <link rel="stylesheet" href="CSS\stylo.css">
+    <script src="JS\script.js"></script>
+</head>
+
+<body>
+s
+    <header>
+        <nav>
+            <ul>
+                <li><a href="portfolio.html">Portfolio</a></li>
+                <li><a href="#Excel">Excel</a></li>
+                <li><a href="#PowerBI">PowerBI</a></li>
+                <li><a href="#Contacto">Contacto</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <h1>"DoniMartins"</h1>
+
+    <section id="portfolio">
+        <h2 data-animate="true">Portfolio</h2>
+    </section>
+
+    <section id="Excel">
+        <h2 data-animate="true">Excel</h2>
+    </section>
+
+    <section id="PowerBI">
+        <h2 data-animate="true">PowerBI</h2>
+    </section>
+
+    <section id="Contacto">
+        <h2 data-animate="true">Contacto</h2>
+    </section>
+
+    <div id="cursor" class="cursor">
+        <div class="ring">
+            <div>
+                <!--Border-->
+            </div>
+        </div>
+        <div class="ring">
+            <div>
+                <!--Pointer-->
+            </div>
+        </div>
+    </div>
+    <footer>
+        <p>&copy; 2023 Meu Portfolio Incrível. Todos os direitos reservados.</p>
+    </footer>
+    <script src="JS\script.js"></script>
+</body>
+
+</html>
